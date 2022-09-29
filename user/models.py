8070244bbbcc29f5ec216cc178b17ccc3902d261
Muhammadamin <muhammadamintojimamatov@gmail.com>
2022-09-29 18:06:19 +0500

    models created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13, blank=True, null=True, unique=True)
+    email = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+class UserRelative(models.Model):
+    user = models.ForeignKey(User, related_name='relative', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13, blank=True, null=True, unique=True)
+    email = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
